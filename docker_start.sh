e9b5c6823a2200fc86d28ec7083195b25f18d318
Your Name <you@example.com>
2025-09-06 13:36:40 +0200

    cleanup

--- docker_start.sh
+++ docker_start.sh
@@ -7,4 +7,4 @@
 #  drojewski/geoaga:latest
 
 #ustawic nazwe pliku w INPUT_FILE!!
-sudo docker run -it --rm -e DISPLAY=$DISPLAY -e XAUTHORITY=/home/drojewski/.Xauthority -v /tmp/.X11-unix/:/tmp/.X11-unix -v /home/drojewski/.Xauthority:/home/drojewski/.Xauthority:ro -v /tmp/asd:/app/auto_walk_screenshots -e INPUT_FILE=part_5.json -v /home/drojewski:/app/input_files drojewski/geoaga:latest
+sudo docker run -it --rm -e DISPLAY=$DISPLAY -e XAUTHORITY=/home/drojewski/.Xauthority -v /tmp/.X11-unix/:/tmp/.X11-unix -v /home/drojewski/.Xauthority:/home/drojewski/.Xauthority:ro -v /tmp/auto_walk_screenshots:/app/auto_walk_screenshots -e INPUT_FILE=part_5.json -v /home/drojewski:/app/input_files drojewski/geoaga:latest
