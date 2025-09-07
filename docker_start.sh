da4cfe143959f27745a127600eaad62d3abe333a
Your Name <you@example.com>
2025-09-07 12:27:50 +0200

    fix docker start

--- docker_start.sh
+++ docker_start.sh
@@ -1,18 +1,11 @@
-#docker run -it --rm \
-#  -e DISPLAY=$DISPLAY \
-#  -e XAUTHORITY=/tmp/Xauthority \
-#  -v /tmp/.X11-unix:/tmp/.X11-unix \
-#  -v /run/user/1000/gdm/Xauthority:/tmp/Xauthority:ro \
-#  -v /tmp/asd:/app/auto_walk_screenshots \
-#  drojewski/geoaga:latest
-
-INPUT_FILE=part_5.json
+INPUT_FILE=part_6.json
 LOG_FILE="log_auto_walk_${INPUT_FILE}.txt"
+XAUTHORITY=/run/user/1000/gdm/Xauthority
 touch ./$LOG_FILE
 xhost +local:
 sudo docker run -it --rm \
   -e DISPLAY=$DISPLAY \
-  -v $HOME/.Xauthority:/tmp/.Xauthority:ro \
+  -v $XAUTHORITY:/tmp/.Xauthority:ro \
   -e XAUTHORITY=/tmp/.Xauthority \
   -v /tmp/.X11-unix/:/tmp/.X11-unix \
   -v ./auto_walk_screenshots:/app/auto_walk_screenshots \
@@ -20,4 +13,3 @@ sudo docker run -it --rm \
   -v ./$INPUT_FILE:/app/$INPUT_FILE \
   -v ./$LOG_FILE:/app/$LOG_FILE \
   drojewski/geoaga:latest
-
