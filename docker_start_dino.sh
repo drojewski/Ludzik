525e44aa001afd7a47418d3f612c827f97975893
Your Name <you@example.com>
2025-09-13 12:17:44 +0200

    adjust docker start dino

--- docker_start_dino.sh
+++ docker_start_dino.sh
@@ -1,1 +1,5 @@
-docker run --gpus all -it --rm drojewski/geoaga:latest python3 /app/6_MainDino.py
+docker run --gpus all -it --rm \
+  -v /home/drojewski/app/auto_walk_screenshots:/app/auto_walk_screenshots \
+  -v /home/drojewski/app/d.jpg:/app/d.jpg \
+  -w /app \
+  drojewski/geoaga:latest python3 6_MainDino.py
